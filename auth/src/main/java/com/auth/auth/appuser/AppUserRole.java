e7b01204ac76e75cb34ce96c387b5a49f9a690e1
kiko0217 <anziskita@gmail.com>
2022-07-25 03:33:10 +0700

    create auth with email sender with devmail loclah server smtp

---
+++ auth/src/main/java/com/auth/auth/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.auth.auth.appuser;
+
+public enum AppUserRole {
+  USER,
+  ADMIN
+}
---
+++ auth/src/main/java/com/auth/auth/appuser/AppUserService.java
@@ -0,0 +1,57 @@
+package com.auth.auth.appuser;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth.auth.registration.token.ConfirmationToken;
+import com.auth.auth.registration.token.ConfirmationTokenService;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class AppUserService implements UserDetailsService {
+
+  private final String USER_NOT_FOUND_MSG = "User with email %s not found";
+  private final AppUserRepository appUserRepository;
+  private final BCryptPasswordEncoder bCryptPasswordEncoder;
+  private final ConfirmationTokenService confirmationTokenService;
+
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    // TODO Auto-generated method stub
+    return appUserRepository.findByEmail(email).orElseThrow(
+        () -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+  }
+
+  public String signUpUser(AppUser appUser) {
+    boolean userExists = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
+
+    if(userExists) {
+      throw new IllegalStateException("email already taken");
+    }
+    String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+    appUser.setPassword(encodedPassword );
+    appUserRepository.save(appUser);
+    String token = UUID.randomUUID().toString();
+    ConfirmationToken confirmationToken = new ConfirmationToken(
+      token,
+      LocalDateTime.now(),
+      LocalDateTime.now().plusMinutes(15),
+      appUser
+    );
+
+    confirmationTokenService.saveConfirmationToken(confirmationToken);
+    return token;
+  }
+  public int enableAppUser(String email) {
+    return appUserRepository.enableAppUser(email);
+  }
+}
